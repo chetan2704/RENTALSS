323a9bfba1862ddd73411e74eae9ec166a3e2613
chetan2704 <mohitechetan67@gmail.com>
2024-08-13 22:08:41 +0530

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PriceComponent } from './price/price.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PriceComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
