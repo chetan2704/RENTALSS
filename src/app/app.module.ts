a5204526cd4e851bc5fb04739bdea902a81e2972
chetan2704 <mohitechetan67@gmail.com>
2024-09-07 21:04:23 +0530

    added help center

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PriceComponent } from './price/price.component';
+import { HelpcentreComponent } from './helpcentre/helpcentre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PriceComponent
+    PriceComponent,
+    HelpcentreComponent
   ],
   imports: [
     BrowserModule
